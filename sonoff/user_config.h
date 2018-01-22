4f4dace92dbcaea0baf2d683202287ea9dbd6b6d
rebel7580 <30353207+rebel7580@users.noreply.github.com>
2018-01-22 14:44:07 -0500

    .

--- sonoff/user_config.h
+++ sonoff/user_config.h
@@ -51,8 +51,8 @@
 #define WIFI_SUBNETMASK        "255.255.255.0"   // [IpAddress3] If not using DHCP set Network mask
 #define WIFI_DNS               "192.168.2.27"    // [IpAddress4] If not using DHCP set DNS IP address (might be equal to WIFI_GATEWAY)
 
-#define STA_SSID1              "beantown"      // [Ssid1] Wifi SSID
-#define STA_PASS1              "ETKNB3WEBY3Q17"  // [Password1] Wifi password
+#define STA_SSID1              ""      // [Ssid1] Wifi SSID
+#define STA_PASS1              ""  // [Password1] Wifi password
 #define STA_SSID2              ""      // [Ssid2] Optional alternate AP Wifi SSID
 #define STA_PASS2              ""  // [Password2] Optional alternate AP Wifi password
 #define WIFI_CONFIG_TOOL       WIFI_WPSCONFIG    // [WifiConfig] Default tool if wifi fails to connect
